9667f5d3fd9d8dbc36d0550cb39f7f56d2fb04ae
yug <yugdesai517@gmail.com>
2025-04-20 11:46:06 +0530

    1234

--- move.py
+++ move.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-#testing 12345678
+#testing 12334
 
 # Detect current user and system
 current_username = getpass.getuser().lower()
